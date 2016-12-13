a604a71bd3b8a1ef649ab5c7b6a7c13d184eb51a
nowe98 <richy_lion@yahoo.com>
2016-12-13 13:58:03 +0700

    menu with choose stage

--- src/scene/MenuScreen.java
+++ src/scene/MenuScreen.java
@@ -19,9 +19,14 @@ import render.RenderableHolder;
 public class MenuScreen extends BorderPane {
 	private Canvas headline;
 	private Canvas blank;
+	private Canvas blank1;
+	private Canvas blank2;
 	private VBox menu;
 	private Button startBtn;
 	private Button exitBtn;
+	private Button stage1Btn;
+	private Button stage2Btn;
+	private Button backBtn;
 	// private Button configBtn;
 	private int width, height;
 
@@ -48,11 +53,23 @@ public class MenuScreen extends BorderPane {
 		
 		blank = new Canvas(768,30);
 		menu.getChildren().add(1,blank);
+		blank1 = new Canvas(768,10);
+		blank2 =new Canvas(768,10);
 		
 		exitBtn = new Button("Exit");
 		exitBtn.setPrefHeight(30);
 		exitBtn.setPrefWidth(120);
 		menu.getChildren().add(2, exitBtn);
+		
+		
+		stage1Btn = new Button("Stage 1");
+		stage1Btn.setPrefSize(120, 30);
+		stage2Btn = new Button("Stage 2");
+		stage2Btn.setPrefSize(120, 30);
+		
+		backBtn =new Button("Back");
+		backBtn.setPrefSize(120, 30);
+		
 		
 		
 
@@ -82,7 +99,14 @@ public class MenuScreen extends BorderPane {
 			@Override
 			public void handle(KeyEvent event) {
 				if (event.getCode() == KeyCode.ENTER) {
-					Main.instance.toggleScene("gameScene");
+					menu.getChildren().clear();
+					menu.getChildren().add(0, stage1Btn);
+					menu.getChildren().add(1,blank1);
+					menu.getChildren().add(2, stage2Btn);
+					menu.getChildren().add(3, blank2);
+					menu.getChildren().add(4, backBtn);
+					//Main.instance.toggleScene("gameScene");
+					
 				}
 
 			}
@@ -92,7 +116,13 @@ public class MenuScreen extends BorderPane {
 
 			@Override
 			public void handle(ActionEvent event) {
-				Main.instance.toggleScene("gameScene");
+				menu.getChildren().clear();
+				menu.getChildren().add(0, stage1Btn);
+				menu.getChildren().add(1,blank1);
+				menu.getChildren().add(2, stage2Btn);
+				menu.getChildren().add(3, blank2);
+				menu.getChildren().add(4, backBtn);
+				//Main.instance.toggleScene("gameScene");
 
 			}
 		});
@@ -116,6 +146,94 @@ public class MenuScreen extends BorderPane {
 
 			}
 		});
+		
+		stage1Btn.setOnKeyPressed(new EventHandler<KeyEvent>() {
+
+			@Override
+			public void handle(KeyEvent event) {
+				if (event.getCode() == KeyCode.ENTER) {
+					Main.instance.toggleScene("gameScene");
+					menu.getChildren().clear();
+					menu.getChildren().add(0,startBtn);
+					menu.getChildren().add(1, blank);
+					menu.getChildren().add(2, exitBtn);
+					
+				}
+
+			}
+		});
+
+		stage1Btn.setOnAction(new EventHandler<ActionEvent>() {
+
+			@Override
+			public void handle(ActionEvent event) {
+				Main.instance.toggleScene("gameScene");
+				menu.getChildren().clear();
+				menu.getChildren().add(0,startBtn);
+				menu.getChildren().add(1, blank);
+				menu.getChildren().add(2, exitBtn);
+
+			}
+		});
+
+		
+		stage2Btn.setOnKeyPressed(new EventHandler<KeyEvent>() {
+
+			@Override
+			public void handle(KeyEvent event) {
+				if (event.getCode() == KeyCode.ENTER) {
+					Main.instance.toggleScene("gameScene");
+					menu.getChildren().clear();
+					menu.getChildren().add(0,startBtn);
+					menu.getChildren().add(1, blank);
+					menu.getChildren().add(2, exitBtn);
+					
+				}
+
+			}
+		});
+
+		stage2Btn.setOnAction(new EventHandler<ActionEvent>() {
+
+			@Override
+			public void handle(ActionEvent event) {
+				Main.instance.toggleScene("gameScene");
+				menu.getChildren().clear();
+				menu.getChildren().add(0,startBtn);
+				menu.getChildren().add(1, blank);
+				menu.getChildren().add(2, exitBtn);
+
+			}
+		});
+		
+		
+		backBtn.setOnKeyPressed(new EventHandler<KeyEvent>() {
+
+			@Override
+			public void handle(KeyEvent event) {
+				if (event.getCode() == KeyCode.ENTER) {
+					menu.getChildren().clear();
+					menu.getChildren().add(0,startBtn);
+					menu.getChildren().add(1, blank);
+					menu.getChildren().add(2, exitBtn);
+					
+				}
+
+			}
+		});
+
+		backBtn.setOnAction(new EventHandler<ActionEvent>() {
+
+			@Override
+			public void handle(ActionEvent event) {
+				menu.getChildren().clear();
+				menu.getChildren().add(0,startBtn);
+				menu.getChildren().add(1, blank);
+				menu.getChildren().add(2, exitBtn);
+
+			}
+		});
+		
 	}
 
 	public void applyResize() {
